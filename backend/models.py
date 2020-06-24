0009b90d39c27ecfed946779e3d53f2654628e8b
Yousef Almassad <yousef.at.tamimi@gmail.com>
2020-06-24 06:39:24 +0300

    Change database variable name

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,15 @@
 import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
-from config import database_setup
+from config import database_info
 import json
 
 
 database_name = "trivia"
-database_path = "postgres://{}/{}".format(database_setup['port'], database_setup["database_name_production"])
+database_path = "postgres://{}/{}".format(database_info['port'], database_info["database_name_production"])
 # You can add additional ditails for the authenticaiton to access your database
 #  with a custom userName and Password like so:
-# database_path = "postgres://{}:{}@{}/{}".format(database_setup["user_name"], database_setup["password"], database_setup["port"], database_setup["database_name_production"])
+# database_path = "postgres://{}:{}@{}/{}".format(database_info["user_name"], database_info["password"], database_setup["port"], database_setup["database_name_production"])
 
 
 db = SQLAlchemy()
@@ -20,7 +20,7 @@ setup_db(app)
 '''
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = database_setup["SQLALCHEMY_TRACK_MODIFICATIONS"]
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = database_info["SQLALCHEMY_TRACK_MODIFICATIONS"]
     db.app = app
     db.init_app(app)
     db.create_all()
